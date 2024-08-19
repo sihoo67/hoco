7ec6e7e74174cc62a3cdc42305db8be340b59d3f
핑구 <58054397+sihoo67@users.noreply.github.com>
2024-08-19 23:16:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
-    const onboarding = document.querySelector('.onboarding');
-    const mainTitle = document.querySelector('.main-title');
-    const infoTopLeft = document.querySelector('.info-top-left');
-    const infoTopRight = document.querySelector('.info-top-right');
-    const infoBottomLeft = document.querySelector('.info-bottom-left');
-    const infoBottomRight = document.querySelector('.info-bottom-right');
+    const scrollTitle = document.querySelector('.scroll-title');
+    const info = document.querySelector('.info');
     const imageGallery = document.querySelector('.image-gallery');
-    const galleryImages = document.querySelectorAll('.gallery-image');
 
-    if (scrollY > 100 && scrollY < 500) {
-        const scale = 1 - (scrollY - 100) / 400;
-        const fontSize = 5 - 2 * (scrollY - 100) / 400;
-
-        mainTitle.style.transform = `scale(${scale})`;
-        mainTitle.style.fontSize = `${fontSize}rem`;
-
-        infoTopLeft.style.opacity = `${(scrollY - 100) / 400}`;
-        infoTopRight.style.opacity = `${(scrollY - 100) / 400}`;
-        infoBottomLeft.style.opacity = `${(scrollY - 100) / 400}`;
-        infoBottomRight.style.opacity = `${(scrollY - 100) / 400}`;
+    // 글씨가 화면 중간에 도달하면 크고 두껍게 나오도록 설정
+    if (scrollY > 100) {
+        scrollTitle.style.opacity = '1';
+        scrollTitle.style.transform = 'translateY(0)';
     }
 
-    if (scrollY >= 500) {
-        onboarding.style.transform = `translateY(-${scrollY - 500}px)`;
-        mainTitle.style.fontSize = `7rem`;
-        infoTopLeft.style.opacity = `1`;
-        infoTopRight.style.opacity = `1`;
-        infoBottomLeft.style.opacity = `1`;
-        infoBottomRight.style.opacity = `1`;
+    // 스크롤 도착 시 정보 나타남
+    if (scrollY > 300) {
+        info.style.opacity = '1';
     }
 
-    if (scrollY > 1000) {
+    // 스크롤 도착 시 이미지 갤러리 나타남
+    if (scrollY > 600) {
         imageGallery.style.opacity = '1';
-        galleryImages.forEach((image, index) => {
-            setTimeout(() => {
-                image.style.opacity = '1';
-            }, index * 200);
-        });
     }
 });
